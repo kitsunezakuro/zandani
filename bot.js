d08454f806aa38715b8df7b4ad82938991d67a37
kitsunezakuro <47758428+kitsunezakuro@users.noreply.github.com>
2019-02-23 12:45:55 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,6 @@
+const discord = require ('discord.js')
+
+var client = new discord.Client();
+
+const token = "NTQ3MDQ5OTc5MTQzNzgyNDAx.D0xIrQ.b_mrhehNh376Xqc7497Hj5o9_eQ";
+const fs = require ("fs");
